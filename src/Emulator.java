ce9802020672b30e54ce28c0880d5e8af572c56c
Marc Rolli <rollimar@students.zhaw.ch>
2025-04-28 12:23:23 +0200

    Implement 1 Step Emulation

--- src/Emulator.java
+++ src/Emulator.java
@@ -1,4 +1,5 @@
 import java.util.List;
+import java.util.Scanner;
 
 public class Emulator {
     private static final int TAPE_SYMBOL_COUNT = 15;
@@ -6,18 +7,42 @@ public class Emulator {
     public static void main(String[] args) {
         List<Transition> transitions = TransitionHandler.getTransitions();
         String tape = TapeHandler.getTape();
-        // TODO: choose step / run mode
-        // TODO: emulation
+        Boolean stepOn = setStepMode();
+        emulate(transitions, tape, stepOn);
     }
 
     private static void emulate(List<Transition> transitions, String tape, boolean stepOn) {
+        int currentState = 1;
+        char currentTapeSymbol = tape.charAt(0);
+        int position = 0;
+        int counter = 0;
+
+        for (Transition transition : transitions) {
+            if (currentState == transition.initialState() && currentTapeSymbol == transition.readTapeSymbol()) {
+                currentState = transition.nextState();
+                tape = replaceAtPosition(tape, position, transition.writeTapeSymbol());
+                switch (transition.moveDirection()) {
+                    case 'l' -> position -= 1;
+                    case 'r' -> position += 1;
+                }
+                counter++;
+            }
+        }
+
+        printInformation(currentState, tape, position, counter);
+    }
+
+    private static String replaceAtPosition(String tape, int position, char symbol) {
+        char[] tapeArray = tape.toCharArray();
+        tapeArray[position] = symbol;
+        return new String(tapeArray);
     }
 
     private static void printInformation(int state, String tape, int position, int counter) {
         StringBuilder sb = new StringBuilder();
 
         // step counter
-        sb.append("Step: " + counter);
+        sb.append("Steps: " + counter);
         sb.append(System.lineSeparator());
 
         // current state & position
@@ -39,5 +64,28 @@ public class Emulator {
         sb.append(System.lineSeparator());
 
         System.out.print(sb);
+    }
+
+    private static boolean setStepMode() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Choose Mode - [S]tep-Mode / [R]un-Mode");
+        System.out.print("Mode: ");
+        while (!sc.hasNext("[SsRr]")) {
+            System.out.println("Not valid - Please enter a valid character (S/s/R/r)");
+            System.out.print("Mode: ");
+            sc.next();
+        }
+        String input = sc.next();
+
+        switch (input) {
+            case "S", "s" -> {
+                return true;
+            }
+            case "R", "r" -> {
+                return false;
+            }
+        }
+        return false;
     }
 }
